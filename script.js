4862b4ec3b7e4b2c90c199fda0e1124e266dc67c
Daniel Frazier <dfrazier.jl@gmail.com>
2023-07-24 11:56:28 -0700

    fif formatting

--- script.js
+++ script.js
@@ -1,66 +1,67 @@
 //multiple arrays of characters for user to pick from
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var specialcharacters = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialcharacters = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 var possiblePassword = []
 var password = []
 
-function getrandomarray (arr) {
-  var randomindex = Math.floor (Math.random()*arr.length)
+function getrandomarray(arr) {
+  var randomindex = Math.floor(Math.random() * arr.length)
   var randomelement = arr[randomindex]
   return randomelement
 }
 
-function generatePassword () {
-//Ask user how many characters the want in their password
-var passwordLength = parseInt( window.prompt("How many characters would you like your password to contain?"));
-if (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-  alert ("must enter a number value, and number must be greater than 8 and less than 128.")
-  return null
-}
-//Ask user if they want special characters in their password
-var useofspecialcharacters = window.confirm ("Click OK to confirm including special characters.");
-if (useofspecialcharacters === true) {
-  possiblePassword = possiblePassword.concat(specialcharacters) 
-}
-console.log(possiblePassword);
-//Ask user if they want numeric characters in their password
-var useofnumericcharacters = window.confirm ("Click OK to confirm including numeric characters.");
-if (useofnumericcharacters === true) {
-  possiblePassword = possiblePassword.concat(numbers)
-}
+function generatePassword() {
+  //Ask user how many characters the want in their password
+  var passwordLength = parseInt(window.prompt("How many characters would you like your password to contain?"));
+  if (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("must enter a number value, and number must be greater than 8 and less than 128.")
+    return null
+  }
+  //Ask user if they want special characters in their password
+  var useofspecialcharacters = window.confirm("Click OK to confirm including special characters.");
+  if (useofspecialcharacters === true) {
+    possiblePassword = possiblePassword.concat(specialcharacters)
+  }
+  console.log(possiblePassword);
+  //Ask user if they want numeric characters in their password
+  var useofnumericcharacters = window.confirm("Click OK to confirm including numeric characters.");
+  if (useofnumericcharacters === true) {
+    possiblePassword = possiblePassword.concat(numbers)
+  }
 
-//Ask user if they want lowercase characters in their password
-var useoflowercasecharacters = window.confirm ("Click OK to confirm including lowercase characters.");
-if (useoflowercasecharacters === true) {
-  possiblePassword = possiblePassword.concat(lowerCaseLetters)
-}
+  //Ask user if they want lowercase characters in their password
+  var useoflowercasecharacters = window.confirm("Click OK to confirm including lowercase characters.");
+  if (useoflowercasecharacters === true) {
+    possiblePassword = possiblePassword.concat(lowerCaseLetters)
+  }
 
-//Ask user if they want uppercase characters in their password
-var useofuppercasecharacters = window.confirm ("Click OK to confirm including uppercase characters.");
-if (useofuppercasecharacters === true) {
-  possiblePassword = possiblePassword.concat(upperCaseLetters)
-}
+  //Ask user if they want uppercase characters in their password
+  var useofuppercasecharacters = window.confirm("Click OK to confirm including uppercase characters.");
+  if (useofuppercasecharacters === true) {
+    possiblePassword = possiblePassword.concat(upperCaseLetters)
+  }
 
-while (!useofspecialcharacters && !useofnumericcharacters && !useoflowercasecharacters && !useofuppercasecharacters) {
-  window.alert ("must pick at least one option.");
-  return null
-}
-for (var i = 0; i < passwordLength; i++) {
-  console.log(passwordLength);
-  console.log (possiblePassword);
-  var newPassword = getrandomarray(possiblePassword)
-  console.log (newPassword);
-  password.push (newPassword)
-}
-return password.join ("")
+  while (!useofspecialcharacters && !useofnumericcharacters && !useoflowercasecharacters && !useofuppercasecharacters) {
+    window.alert("must pick at least one option.");
+    return null
+  }
+
+  for (var i = 0; i < passwordLength; i++) {
+    console.log(passwordLength);
+    console.log(possiblePassword);
+    var newPassword = getrandomarray(possiblePassword)
+    console.log(newPassword);
+    password.push(newPassword)
+  }
+
+  return password.join("")
+
 }
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -68,7 +69,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
