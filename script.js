07e26faa18992ee9507f993e300323e1fe5c8c25
Daniel Frazier <dfrazier.jl@gmail.com>
2023-07-24 09:06:01 -0700

    Add function

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specialcharacters = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
-
+var possiblePassword = []
+function generatePassword () {
 //Ask user how many characters the want in their password
 var passwordLength = window.prompt("How many characters would you like your password to contain?");
 
@@ -18,7 +19,7 @@ var useoflowercasecharacters = window.confirm ("Click OK to confirm including lo
 
 //Ask user if they want uppercase characters in their password
 var useofuppercasecharacters = window.confirm ("Click OK to confirm including uppercase characters.");
-
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
